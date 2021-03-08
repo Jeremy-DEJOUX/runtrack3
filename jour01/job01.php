d3ed55266f0c51b3748e1d6f4f73253c8b93743d
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-08 14:51:42 +0100

    premier job

---
+++ jour01/job01.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>JOB 01</title>
+    <script>
+        alert("Hello");
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
