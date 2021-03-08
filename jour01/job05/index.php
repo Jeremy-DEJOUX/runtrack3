2cb6fc7fcc59b8511c6595f02e075a3a699b754b
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-08 16:41:56 +0100

    job05 done

---
+++ jour01/job05/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>JOB 01</title>
+    <script type="text/javascript" src="script.js"></script>
+</head>
+
+<body>
+
+</body>
+
+</html>
---
+++ jour01/job05/script.js
@@ -0,0 +1,5 @@
+jourssemaines = new Array('Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche');
+
+for (let index = 0; index < jourssemaines.length; index++) {
+    document.write(jourssemaines[index] + " ");
+}
