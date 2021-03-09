9473e9ea8f5aa01f7eb99fe45b77ee74e3522715
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-09 18:18:59 +0100

    JOB03 Done

--- jour02/job03/index.php
+++ jour02/job03/index.php
@@ -4,11 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <title>JOB 01</title>
-    <script type="text/javascript" src="script.js"></script>
+
 </head>
 
 <body>
+    <button id="button">Hi</button>
 
+    <article id="compteur">
+        0
+    </article>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 
 </html>
