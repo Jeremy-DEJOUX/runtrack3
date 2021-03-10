4ae890ae0dacc4e7c2fbf7708321dcd132e1fb42
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-10 18:30:56 +0100

    job1 done job02 almost done

---
+++ jour03/job01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Job 01</title>
+</head>
+
+<body>
+    <button id="button">SHOW</button>
+    <button id="hide_button">HIDE</button>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
---
+++ jour03/job02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style.css">
+    <title>Job 2</title>
+</head>
+
+<body>
+    <div id="shuffled">
+        <img class="img" id="1" src="img/arc1.png">
+        <img class="img" id="2" src="img/arc2.png">
+        <img class="img" id="3" src="img/arc3.png">
+        <img class="img" id="4" src="img/arc4.png">
+        <img class="img" id="5" src="img/arc5.png">
+        <img class="img" id="6" src="img/arc6.png">
+    </div>
+    <button id="button"></button>
+    <div id="rangees"></div>
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
