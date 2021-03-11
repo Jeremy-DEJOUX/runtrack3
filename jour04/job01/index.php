e36022acaaef8f697fb4d10f3d83be73f5b7f1bd
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-11 10:58:12 +0100

    job01 Done

---
+++ jour04/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 01</title>
+</head>
+
+<body>
+    <button id="button">Start</button>
+
+
+    <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript" src="script.js"></script>
+</body>
+
+</html>
---
+++ jour04/job01/script.js
@@ -0,0 +1,11 @@
+$("#button").click(function () {
+    $.ajax({
+        url: "expression.txt",
+        method: "GET",
+        DataType: "text"
+    })
+        .done(function(msg){
+            console.log(msg);
+            $("body").append("<p>"+msg+"</p>")
+        })
+})
