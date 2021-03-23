5b071dd4804941cfbfbe2e936aa3bad6e7ad9aaa
DEJOUX Jeremy <jeremy.dejoux@laplateforme.io>
2021-03-23 10:48:35 +0100

    Changement mineur

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,11 +1,7 @@
-$(document).ready(function (){
-
-
-    function jsonValuekey(json, key){
-        var obj = JSON.parse(json);
-        return obj[key];
-    }
-
-    var json = '{"name": "La Plateforme_", "address": "8 rue d hozier", "city": "Marseille", "nb_staff": 11, "creation":2019}';
-    jsonValuekey(json, 'city');
-});
+    function jsonValueKey(json, key) {
+    let platform = JSON.parse(json);
+    console.log(platform[key]);
+    return platform[key];
+}
+    var json = '{"name":"La Plateforme_", "address":"8 rue d hozier", "city": "Marseille", "nb_staff":"11", "creation":"2019"}'; 
+    jsonValueKey(json, 'city');
